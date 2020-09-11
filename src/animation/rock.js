7c66bafcffd56f6a6b17d9de5f8c3489c12ff101
Theophile Avoyne <tavoyne@gmail.com>
2020-09-11 16:45:57 +0200

    Rock is now spinning and every face has a random color

--- src/animation/rock.js
+++ src/animation/rock.js
@@ -1,11 +1,15 @@
 import { Mesh, MeshPhongMaterial, SphereGeometry } from 'three';
 
-const color = 0x0f0f0f;
 const heightSegments = 16;
 const radius = 40;
+const vertexColors = true;
 const widthSegments = 16;
 
 const geometry = new SphereGeometry(radius, widthSegments, heightSegments);
-const material = new MeshPhongMaterial({ color });
+const material = new MeshPhongMaterial({ vertexColors });
+
+geometry.faces.forEach((face) => {
+  face.color.setRGB(Math.random(), Math.random(), Math.random());
+});
 
 export default new Mesh(geometry, material);
--- src/hooks/useAnim.js
+++ src/hooks/useAnim.js
@@ -1,3 +1,4 @@
+import { gsap } from 'gsap';
 import { useEffect } from 'react';
 
 import camera from '../animation/camera';
@@ -10,7 +11,24 @@ export default (canvasRef) => {
   useEffect(() => {
     scene.add(light);
     scene.add(rock);
+
     const renderer = createRenderer(canvasRef.current);
-    renderer.render(scene, camera);
+
+    const render = () => { renderer.render(scene, camera); };
+
+    const animate = () => {
+      render();
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    gsap.to(rock.rotation, {
+      duration: 10,
+      ease: 'none',
+      repeat: -1,
+      x: Math.PI * 2,
+      y: Math.PI * 2,
+    });
   }, []);
 };
