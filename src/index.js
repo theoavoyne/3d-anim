0b0baa070d9e37db2b65700bcd48f90bf5b879cb
Theophile Avoyne <tavoyne@gmail.com>
2020-09-11 12:37:34 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'normalize.css/normalize.css';
+import './static/styles/base.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
