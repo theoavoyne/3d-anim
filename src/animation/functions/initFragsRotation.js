29ec9539831b850616c03bf71b3ae8db9e7d7dba
Theophile Avoyne <tavoyne@gmail.com>
2020-09-25 11:11:24 +0200

    Fragments are now rotation altogether

--- src/animation/functions/initFragsRotation.js
+++ src/animation/functions/initFragsRotation.js
@@ -1,12 +1,18 @@
-const delta = Math.PI / 10;
+/* eslint-disable no-param-reassign */
+
+const individualDelta = Math.PI / 8;
+const groupDeltaX = Math.PI / 128;
+const groupDeltaY = Math.PI / 64;
 
 export default (fragments) => {
   const update = (timeDelta) => {
     fragments.children.forEach((fragment) => {
-      const angle = delta * (timeDelta / 1000);
+      const angle = individualDelta * (timeDelta / 1000);
       const axis = fragment.userData.rotationAxis;
       fragment.rotateOnAxis(axis, angle);
     });
+    fragments.rotation.x += groupDeltaX * (timeDelta / 1000);
+    fragments.rotation.y += groupDeltaY * (timeDelta / 1000);
   };
 
   return { update };
