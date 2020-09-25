a93657822b9e7e3725bbe8707fe9f0a059edfc63
Theophile Avoyne <tavoyne@gmail.com>
2020-09-25 11:28:01 +0200

    Minor

--- src/animation/objects/createDoor.js
+++ src/animation/objects/createDoor.js
@@ -2,7 +2,7 @@ import { Sprite, SpriteMaterial, TextureLoader } from 'three';
 
 import Particle100PNG from '../../static/images/100.png';
 
-const scale = 80;
+const scale = 78;
 
 export default () => {
   const material = new SpriteMaterial({
