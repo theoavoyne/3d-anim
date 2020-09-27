b113b484d05eebf88d0c409d8b2eb414bc1c05a9
Theophile Avoyne <tavoyne@gmail.com>
2020-09-27 20:57:42 +0200

    Animating rock appearance

--- src/animation/functions/initTransition1.js
+++ src/animation/functions/initTransition1.js
@@ -1,3 +1,10 @@
+import { gsap, Power2 } from 'gsap';
+
+import { position as cameraPosition } from '../objects/createCamera';
+
+const tweenDelay = 1;
+const tweenDuration = 2;
+
 export default (args) => {
   const {
     cameraRotationMM,
@@ -23,10 +30,22 @@ export default (args) => {
   return () => {
     setStep(1);
 
-    handlers.MD = [facesDispMD, progressMD, rockRotationMD, rockScaleMD];
-    handlers.MM = [cameraRotationMM, facesDispMM, rockRotationMM];
-    handlers.MU = [facesDispMU, progressMU, rockRotationMU, rockScaleMU];
     handlers.updaters = [updateRockRotation];
+
+    gsap.fromTo(
+      rock.position,
+      { z: cameraPosition[2] },
+      {
+        delay: tweenDelay,
+        duration: tweenDuration,
+        ease: Power2.easeOut,
+        z: 0,
+      },
+    ).then(() => {
+      handlers.MD = [facesDispMD, progressMD, rockRotationMD, rockScaleMD];
+      handlers.MM = [cameraRotationMM, facesDispMM, rockRotationMM];
+      handlers.MU = [facesDispMU, progressMU, rockRotationMU, rockScaleMU];
+    });
 
     scene.add(light);
     scene.add(particles);
